751627eed2e0de4127dd52f89970bed9986fe57b
Ben Gottlieb <ben@standalone.com>
2012-06-27 08:10:43 -0500

    fixed some #ifdef and #import ordering issues

--- Foundation/NSData+MD5.m
+++ Foundation/NSData+MD5.m
@@ -8,38 +8,39 @@
 
 #import "NSdata+MD5.h"
 
-@implementation NSData (MD5)
 
 #if TARGET_OS_IPHONE
-#import <CommonCrypto/CommonDigest.h> // Need to import for CC_MD5 access
+	#import <CommonCrypto/CommonDigest.h> // Need to import for CC_MD5 access
 
-- (NSString*) md5Hash
-{
-    unsigned char result[16];
-    CC_MD5( self.bytes, self.length, result ); // This is the md5 call
-    return [NSString stringWithFormat:
-			@"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
-			result[0], result[1], result[2], result[3], 
-			result[4], result[5], result[6], result[7],
-			result[8], result[9], result[10], result[11],
-			result[12], result[13], result[14], result[15]
-			];  
-}
+	@implementation NSData (MD5)
+	- (NSString*) md5Hash
+	{
+		unsigned char result[16];
+		CC_MD5( self.bytes, self.length, result ); // This is the md5 call
+		return [NSString stringWithFormat:
+				@"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
+				result[0], result[1], result[2], result[3], 
+				result[4], result[5], result[6], result[7],
+				result[8], result[9], result[10], result[11],
+				result[12], result[13], result[14], result[15]
+				];  
+	}
 #else
-#import "NSTask+Synchronicity.h"
-#import "NSString+Additions.h"
+	#import "NSTask+Synchronicity.h"
+	#import "NSString+Additions.h"
 
-- (NSString *) md5Hash {
-	NSString					*path = [NSString tempFileNameWithSeed: @"md5" ofType: @"txt"];
-	
-	[self writeToFile: path atomically: YES];
-	NSString					*md5 = [NSTask runTaskWithLaunchPath: @"/sbin/md5" arguments: [NSArray arrayWithObjects: @"-r", path, nil]];
-	
-	@try {
-		md5 = [[md5 componentsSeparatedByString: @" "] objectAtIndex: 0];
-	} @catch (NSException * e) {}
-	
-	return md5;	
-}
+	@implementation NSData (MD5)
+	- (NSString *) md5Hash {
+		NSString					*path = [NSString tempFileNameWithSeed: @"md5" ofType: @"txt"];
+		
+		[self writeToFile: path atomically: YES];
+		NSString					*md5 = [NSTask runTaskWithLaunchPath: @"/sbin/md5" arguments: [NSArray arrayWithObjects: @"-r", path, nil]];
+		
+		@try {
+			md5 = [[md5 componentsSeparatedByString: @" "] objectAtIndex: 0];
+		} @catch (NSException * e) {}
+		
+		return md5;	
+	}
 #endif
 @end
