a8899a6c6c309ed3c1d31319a420f82ec5e2702e
Ben Gottlieb <ben@standalone.com>
2014-05-27 12:22:49 -0500

    improved gradient drawing

--- Foundation/CGContextRef_additions.m
+++ Foundation/CGContextRef_additions.m
@@ -9,13 +9,29 @@
 #import "CGContextRef_additions.h"
 
 void	CGContextDrawRadialGradientInRect(CGContextRef ctx, CGRect rect, UIColor *innerColor, UIColor *outerColor) {
-	CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-	NSArray* colors = @[ (id) innerColor.CGColor, (id) outerColor.CGColor ];
-	CGFloat locations[] = {.25, .75};
-	CGGradientRef gradient = CGGradientCreateWithColors(colorSpace, (CFArrayRef)colors, locations);
-	CGPoint center = (CGPoint){rect.size.width / 2, rect.size.height  / 2};
-	CGContextDrawRadialGradient(ctx, gradient, center, 0, center, MAX(rect.size.width, rect.size.height), kCGGradientDrawsAfterEndLocation);
+	CGFloat				locations[2] = {0.0f, 1.0f};
+	size_t				locationsCount = DIM(locations);
+	CGFloat				colors[8] = {0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,0.75f};
 	
-	CGGradientRelease(gradient);
+	CGColorSpaceRef		colorSpace = CGColorSpaceCreateDeviceRGB();
+	CGGradientRef		gradient = CGGradientCreateWithColorComponents(colorSpace, colors, locations, locationsCount);
+	
 	CGColorSpaceRelease(colorSpace);
+
+	CGPoint				center = CGPointMake(rect.size.width/2, rect.size.height/2);
+	float				radius = MIN(rect.size.width , rect.size.height) ;
+
+	CGContextDrawRadialGradient (ctx, gradient, center, 0, center, radius, kCGGradientDrawsAfterEndLocation);
+	CGGradientRelease(gradient);
+
+	
+//	CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+//	NSArray* colors = @[ (id) innerColor.CGColor, (id) outerColor.CGColor ];
+//	CGFloat locations[] = {.25, .75};
+//	CGGradientRef gradient = CGGradientCreateWithColors(colorSpace, (CFArrayRef)colors, locations);
+//	CGPoint center = (CGPoint){rect.size.width / 2, rect.size.height  / 2};
+//	CGContextDrawRadialGradient(ctx, gradient, center, 0, center, MAX(rect.size.width, rect.size.height), kCGGradientDrawsAfterEndLocation);
+//	
+//	CGGradientRelease(gradient);
+//	CGColorSpaceRelease(colorSpace);
 }
