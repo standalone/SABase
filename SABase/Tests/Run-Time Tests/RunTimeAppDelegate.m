99b3b14b2b335e541b80ebfb82fb888526ffb857
Ben Gottlieb <ben@Elliptical-iMac-4.local>
2014-10-04 06:08:59 -0500

    added some code for threaded connection testing

--- SABase/Tests/Run-Time Tests/RunTimeAppDelegate.m
+++ SABase/Tests/Run-Time Tests/RunTimeAppDelegate.m
@@ -9,7 +9,11 @@
 #import "RunTimeAppDelegate.h"
 #import "TestController.h"
 #import "SA_CustomAlert.h"
+#import "SA_ConnectionQueue.h"
 
+@interface TestingAppDelegate () <SA_ConnectionDelegate>
+@property (nonatomic, strong) NSMutableArray *queues;
+@end
 
 @implementation TestingAppDelegate
 @synthesize window, navigationController;
@@ -20,8 +24,36 @@
 
 	[self.window makeKeyAndVisible];
 	
+	[self stressTestConnections];
+	
 //	[UIWindow alertWindow];
 //	[SA_CustomAlert showAlertWithTitle: @"Test Alert" message: @"Alert Message"];
 	
+}
+
+- (void) stressTestConnections {
+	self.queues = [NSMutableArray new];
+	for (int i = 0; i < 30; i++) {
+		NSOperationQueue				*queue = [NSOperationQueue new];
+		
+		queue.maxConcurrentOperationCount = 1;
+		queue.name = $S(@"%d", i + 1);
+		[self.queues addObject: queue];
+		
+		[queue addOperationWithBlock: ^{
+			int			number = 1;
+			
+			for (int j = 0; j < 100; j++) {
+				[[SA_ConnectionQueue sharedQueue] queueConnection: [SA_Connection connectionWithURL: $U(@"http://example.org/%d/%d", i, j) completionBlock: ^(SA_Connection *incoming, NSInteger resultCode, id error) {
+					
+					[[SA_ConnectionQueue sharedQueue] isExistingConnectionTaggedWith: @"df" delegate: self completion: ^(BOOL value) {
+						
+					}];
+					LOG(@"Finished at #%d on %d", number, i);
+				}]];
+				number++;
+			}
+		}];
+	}
 }
 @end
