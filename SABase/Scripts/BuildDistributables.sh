f96ea5055dc8e18e20271d6de75440a0fc1dd4cf
Ben Gottlieb <ben@standalone.com>
2014-11-06 18:45:52 -0600

    no longer build standard .a files, just the framework

--- SABase/Scripts/BuildDistributables.sh
+++ SABase/Scripts/BuildDistributables.sh
@@ -1,4 +1,4 @@
-LIB_DESTINATION="./SA_Base Library"
+#LIB_DESTINATION="./SA_Base Library"
 
 #rm -rf "${HOME}/Library/Developer/Xcode/DerivedData"
 
@@ -9,19 +9,19 @@ for DERIVED_FOLDER in ${DERIVED}; do
 done
 
 
-echo "Lipo'ing ${LIB_DESTINATION}"
+#echo "Lipo'ing ${LIB_DESTINATION}"
 
-mkdir -p "${LIB_DESTINATION}"
-mkdir -p "${LIB_DESTINATION}/Headers"
+#mkdir -p "${LIB_DESTINATION}"
+#mkdir -p "${LIB_DESTINATION}/Headers"
 
-lipo -create "build/Release-iphoneos/libSABase.a" "build/Debug-iphonesimulator/libSABase.a" -output "${LIB_DESTINATION}/libSABase.a"
+#lipo -create "build/Release-iphoneos/libSABase.a" "build/Debug-iphonesimulator/libSABase.a" -output "${LIB_DESTINATION}/libSABase.a"
 #lipo -create "build/Release-iphoneos/libSABase.a" "build/Debug-iphonesimulator/libSABase.a" -output "${LIB_DESTINATION}/libSABaseD.a"
 
-HEADERS="build/Debug-iphonesimulator/usr/local/include/*.h"
-for HEADER in ${HEADERS}; do
-	FILENAME="${HEADER##*/}"
-	cp $HEADER "${LIB_DESTINATION}/Headers/${FILENAME}" 
-done
+#HEADERS="build/Debug-iphonesimulator/usr/local/include/*.h"
+#for HEADER in ${HEADERS}; do
+#	FILENAME="${HEADER##*/}"
+#	cp $HEADER "${LIB_DESTINATION}/Headers/${FILENAME}"
+#done
 
 
 LIB_DESTINATION="./SA_Base.framework"
