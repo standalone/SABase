8932f056f73bd50c5f918732f6f06cd6de666c4e
Ben Gottlieb <ben@standalone.com>
2017-08-15 17:39:19 -0500

    added macOS 10.11 and 10.12 checkers

--- Foundation/SA_Utilities.h
+++ Foundation/SA_Utilities.h
@@ -176,7 +176,10 @@ extern const NSUInteger				g_sa_base_version;
 	#define				RUNNING_ON_40					NO
     #define				RUNNING_ON_50					NO
     #define				RUNNING_ON_60					NO
-    #define				RUNNING_ON_70					NO
+	#define				RUNNING_ON_70					NO
+
+	#define				RUNNING_ON_10_11				([[NSProcessInfo processInfo] operatingSystemVersion].minorVersion >= 11)
+	#define				RUNNING_ON_10_12				([[NSProcessInfo processInfo] operatingSystemVersion].minorVersion >= 12)
     #define				MULTITASKING_AVAILABLE			NO
 	#define				MAJOR_OS_VERSION				10
 	#define				GCD_AVAILABLE					YES
